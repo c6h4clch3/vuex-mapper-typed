ecbe6e1d5a09fa19ce5a5e4e88a5e0d37fd26f09
c6h4clch3 <c6h4clch3@gmail.com>
2019-06-19 15:37:10 +0900

    fix: テスト修正

--- test/package.test.ts
+++ test/package.test.ts
@@ -16,11 +16,17 @@ const modular = buildModule({
   actions: {
     modA({ commit }, num: number) {
       commit("mutateA", num);
+    },
+    modB({ commit }, str: string) {
+      commit("mutateB", str);
     }
   },
   mutations: {
     mutateA(state, num: number) {
       state.a = num;
+    },
+    mutateB(state, str: string) {
+      state.b = str;
     }
   },
   getters: {
@@ -40,10 +46,68 @@ const namespacedModular = buildModule({
 const MODULE_IDENTIFIER = "test";
 describe("Non-Namespaced store", () => {
   const mappers = makeMappers(modular);
-  const mappedState = mappers.mapStateWithType(["a", "b"]);
+  const mappedState = mappers.mapStateWithType(["a"]);
   const mappedActions = mappers.mapActionsWithType(["modA"]);
   const mappedMutations = mappers.mapMutationsWithType(["mutateA"]);
-  const mappedGetters = mappers.mapGettersWithType(["strA", "numberB"]);
+  const mappedGetters = mappers.mapGettersWithType(["strA"]);
+
+  const component = localVue.extend({
+    template: "<div></div>",
+    computed: {
+      ...mappedState,
+      ...mappedGetters
+    },
+    methods: {
+      ...mappedActions,
+      ...mappedMutations
+    }
+  });
+
+  let store: Store<any>;
+  let wrapper: Wrapper<any>;
+  let vm: any;
+  beforeEach(() => {
+    store = new Vuex.Store({ ...modular });
+    wrapper = shallowMount(component, {
+      store,
+      localVue
+    });
+    vm = wrapper.vm;
+  });
+  it('has property "a" is 0', () => {
+    expect(vm.a).to.eq(0);
+  });
+  it('have no property "b"', () => {
+    expect(vm.b).to.eq(undefined);
+  });
+  it('has getter "strA" is "0"', () => {
+    expect(vm.strA).to.eq("0");
+  });
+  it('has no getter "numberB"', () => {
+    expect(vm.numberB).to.eq(undefined);
+  });
+  it('has action "modA"', async () => {
+    await vm.modA(2);
+    expect(vm.a).to.eq(2);
+  });
+  it('has no action "modB"', async () => {
+    expect(vm.modB).to.eq(undefined);
+  });
+  it('has mutation "mutateA"', () => {
+    vm.mutateA(3);
+    expect(vm.a).to.eq(3);
+  });
+  it('has no mutation "mutateB"', () => {
+    expect(vm.mutateB).to.eq(undefined);
+  });
+});
+
+describe("Non-Namespaced store, mapped with all of properties", () => {
+  const mappers = makeMappers(modular);
+  const mappedState = mappers.mapStateWithType();
+  const mappedActions = mappers.mapActionsWithType();
+  const mappedMutations = mappers.mapMutationsWithType();
+  const mappedGetters = mappers.mapGettersWithType();
 
   const component = localVue.extend({
     template: "<div></div>",
@@ -84,9 +148,17 @@ describe("Non-Namespaced store", () => {
     await vm.modA(2);
     expect(vm.a).to.eq(2);
   });
+  it('has action "modB"', async () => {
+    await vm.modB("4321");
+    expect(vm.b).to.eq("4321");
+  });
   it('has mutation "mutateA"', () => {
     vm.mutateA(3);
     expect(vm.a).to.eq(3);
+  });
+  it('has mutation "mutateB"', () => {
+    vm.mutateB("123456");
+    expect(vm.b).to.eq("123456");
   });
 });
 
@@ -94,7 +166,7 @@ describe("Namespaced store", () => {
   const namespacedMappers = makeMappers(namespacedModular);
   const namespacedMappedState = namespacedMappers.mapStateWithType(
     MODULE_IDENTIFIER,
-    ["a", "b"]
+    ["a"]
   );
   const namespacedMappedActions = namespacedMappers.mapActionsWithType(
     MODULE_IDENTIFIER,
@@ -106,7 +178,75 @@ describe("Namespaced store", () => {
   );
   const namespacedMappedGetters = namespacedMappers.mapGettersWithType(
     MODULE_IDENTIFIER,
-    ["strA", "numberB"]
+    ["strA"]
+  );
+  const namespacedMappedComponent = localVue.extend({
+    template: "<div></div>",
+    computed: {
+      ...namespacedMappedState,
+      ...namespacedMappedGetters
+    },
+    methods: {
+      ...namespacedMappedActions,
+      ...namespacedMappedMutations
+    }
+  });
+  let store: Store<any>;
+  let wrapper: Wrapper<any>;
+  let vm: any;
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        [MODULE_IDENTIFIER]: namespacedModular
+      }
+    });
+    wrapper = shallowMount(namespacedMappedComponent, {
+      store,
+      localVue
+    });
+    vm = wrapper.vm;
+  });
+  it('has property "a" is 0', () => {
+    expect(vm.a).to.eq(0);
+  });
+  it('has no property "b"', () => {
+    expect(vm.b).to.eq(undefined);
+  });
+  it('has getter "strA" is "0"', () => {
+    expect(vm.strA).to.eq("0");
+  });
+  it('has no getter "numberB"', () => {
+    expect(vm.numberB).to.eq(undefined);
+  });
+  it('has action "modA"', async () => {
+    await vm.modA(2);
+    expect(vm.a).to.eq(2);
+  });
+  it('has no action "modB"', async () => {
+    expect(vm.modB).to.eq(undefined);
+  });
+  it('has mutation "mutateA"', () => {
+    vm.mutateA(3);
+    expect(vm.a).to.eq(3);
+  });
+  it('has no mutation "mutateB"', () => {
+    expect(vm.mutateB).to.eq(undefined);
+  });
+});
+
+describe("Namespaced store, mapped with all of properties", () => {
+  const namespacedMappers = makeMappers(namespacedModular);
+  const namespacedMappedState = namespacedMappers.mapStateWithType(
+    MODULE_IDENTIFIER
+  );
+  const namespacedMappedActions = namespacedMappers.mapActionsWithType(
+    MODULE_IDENTIFIER
+  );
+  const namespacedMappedMutations = namespacedMappers.mapMutationsWithType(
+    MODULE_IDENTIFIER
+  );
+  const namespacedMappedGetters = namespacedMappers.mapGettersWithType(
+    MODULE_IDENTIFIER
   );
   const namespacedMappedComponent = localVue.extend({
     template: "<div></div>",
@@ -150,8 +290,16 @@ describe("Namespaced store", () => {
     await vm.modA(2);
     expect(vm.a).to.eq(2);
   });
+  it('has action "modB"', async () => {
+    await vm.modB("4321");
+    expect(vm.b).to.eq("4321");
+  });
   it('has mutation "mutateA"', () => {
     vm.mutateA(3);
     expect(vm.a).to.eq(3);
+  });
+  it('has mutation "mutateB"', () => {
+    vm.mutateB("123456");
+    expect(vm.b).to.eq("123456");
   });
 });
